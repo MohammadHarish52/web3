2eb90d36aa26cd1b4c4fbfc1598eff11be2692ff
tachyonok <tachyonparticle123@gmail.com>
2025-01-16 11:22:34 +0530

    safemath

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -1,38 +1,51 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./zombiefeeding.sol";
+import "./ownable.sol";
+import "./safemath.sol";
 
-contract ZombieHelper is ZombieFeeding {
+contract ZombieFactory is Ownable {
 
-  // 1. Define levelUpFee here
+  using SafeMath for uint256;
+  using SafeMath32 for uint32;
+  using SafeMath16 for uint16;
 
-  modifier aboveLevel(uint _level, uint _zombieId) {
-    require(zombies[_zombieId].level >= _level);
-    _;
+  event NewZombie(uint zombieId, string name, uint dna);
+
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+  uint cooldownTime = 1 days;
+
+  struct Zombie {
+    string name;
+    uint dna;
+    uint32 level;
+    uint32 readyTime;
+    uint16 winCount;
+    uint16 lossCount;
   }
 
-  // 2. Insert levelUp function here
+  Zombie[] public zombies;
 
-  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    zombies[_zombieId].name = _newName;
+  mapping (uint => address) public zombieToOwner;
+  mapping (address => uint) ownerZombieCount;
+
+  function _createZombie(string memory _name, uint _dna) internal {
+    uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
+    zombieToOwner[id] = msg.sender;
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+    emit NewZombie(id, _name, _dna);
   }
 
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    zombies[_zombieId].dna = _newDna;
+  function _generateRandomDna(string memory _str) private view returns (uint) {
+    uint rand = uint(keccak256(abi.encodePacked(_str)));
+    return rand % dnaModulus;
   }
 
-  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
-    uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    uint counter = 0;
-    for (uint i = 0; i < zombies.length; i++) {
-      if (zombieToOwner[i] == _owner) {
-        result[counter] = i;
-        counter++;
-      }
-    }
-    return result;
+  function createRandomZombie(string memory _name) public {
+    require(ownerZombieCount[msg.sender] == 0);
+    uint randDna = _generateRandomDna(_name);
+    randDna = randDna - randDna % 100;
+    _createZombie(_name, randDna);
   }
 
 }
