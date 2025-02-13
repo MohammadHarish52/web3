a0d9c0cb54d604086f4a21b05ca53c0bcf153f83
tachyonok <tachyonparticle123@gmail.com>
2025-02-13 23:24:16 +0530

    rust on steroids p1

---
+++ rust/rust_solana/src/main.rs
@@ -0,0 +1,12 @@
+// syntax and basic data types
+
+fn main() {
+    let num: u64 = 18;
+    let name: String = String::from("Busty chick");
+    let active: bool = true;
+    let vec: Vec<u64> = vec![1, 2, 3, 4, 5];
+    println!("num: {}", num);
+    println!("name: {}", name);
+    println!("active: {}", active);
+    println!("vec: {:?}", vec);
+}
