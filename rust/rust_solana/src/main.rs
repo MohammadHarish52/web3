1a696b6e98dd3d8a3b8b82c62bcf0429e926ee80
tachyonok <tachyonparticle123@gmail.com>
2025-02-14 10:10:39 +0530

    rust on steroids p4

--- rust/rust_solana/src/main.rs
+++ rust/rust_solana/src/main.rs
@@ -1,31 +1,11 @@
-// syntax and basic data types
+// ownership and borrowing
 
-struct Product {
-    name: String,
-    price: u64,
+fn len_string(s: &String) -> usize {
+    s.len()
 }
 
-fn calculate_total_price(products: Vec<Product>) -> u64 {
-    let mut total: u64 = 0;
-    for p in products {
-        total += p.price;
-    }
-    total
-}
 fn main() {
-    let product1 = Product {
-        name: "product1".to_string(),
-        price: 100,
-    };
-    let product2 = Product {
-        name: "product2".to_string(),
-        price: 200,
-    };
-    let product3 = Product {
-        name: "product3".to_string(),
-        price: 300,
-    };
-    let products = vec![product1, product2, product3];
-    let total = calculate_total_price(products);
-    println!("Total price: {}", total);
+    let s = String::from("Big doobs");
+    let le = len_string(&s);
+    println!("The length of {}", le);
 }
