2842f09ce08ed19c414eb013be3bdcba479c17f6
tachyonok <tachyonparticle123@gmail.com>
2025-07-08 11:35:52 +0530

    done with rust hash

--- rust/hashingrust/src/main.rs
+++ rust/hashingrust/src/main.rs
@@ -1,13 +1,13 @@
-
-use sha2::{Sha256,Digest}
 use hex;
+use sha2::{Digest, Sha256};
 
-fn main(){
-    let data  = b"Hello Harish how about learning solana";
+fn main() {
+    let data = b"Hey";
     let mut hasher = Sha256::new();
+
     hasher.update(data);
 
     let result = hasher.finalize();
 
-    println!("Sha256:{}", hex::encode(result));
+    println!("Sha256 {} ", hex::encode(result));
 }
