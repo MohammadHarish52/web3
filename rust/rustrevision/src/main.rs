b648ae2ba483a1fa5096011330949d7a53af1a93
tachyonok <tachyonparticle123@gmail.com>
2025-07-17 14:06:48 +0530

    difference bw stack and heap

--- rust/rustrevision/src/main.rs
+++ rust/rustrevision/src/main.rs
@@ -1,27 +1,8 @@
-enum UserRole {
-    Admin,
-    Editor,
-    Viewer,
-}
-
-fn describe(role: UserRole) {
-    match role {
-        UserRole::Admin => {
-            println!("User is Admin");
-        }
-        UserRole::Editor => {
-            println!("USer is editor")
-        }
-        UserRole::Viewer => {
-            println!("user is viewer")
-        }
-    }
-}
+// this is stack and & points to reference so no error is thrown
 
 fn main() {
-    let user1 = UserRole::Admin;
-    let user2 = UserRole::Editor;
-
-    describe(user1);
-    describe(user2);
+    let name = "Harish";
+    let new_name = name;
+    print!("{}", name);
+    println!("{}", new_name);
 }
