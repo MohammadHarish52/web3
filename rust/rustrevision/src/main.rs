2e64b90c8301788791c1a081d4371dec8afb89e3
tachyonok <tachyonparticle123@gmail.com>
2025-07-18 13:34:18 +0530

    tweet cli

--- rust/rustrevision/src/main.rs
+++ rust/rustrevision/src/main.rs
@@ -1,27 +1,63 @@
-// this is stack and & points to reference so no error is thrown
-// mutable and immutable borrowing can't co exits
+use std::io;
 
-fn main() {
-    let mut s = String::from("Harish");
-
-    let r1 = &s;
-    let r2 = &s;
-
-    println!("{} {}", r1, r2);
-
-    let r3 = &mut s;
-
-    println!("{}", r3);
+enum TweetType {
+    Regular,
+    Excited,
+    Question,
 }
 
-// fn main() {
-//     let mut s = String::from("Harish");
+let tweet_meta = TweetMeta {
+    original: tweet.clone(),
+    tweet_type,
+    length: tweet.len(),
+};
 
-//     let r1 = &s;
-//     let r2 = &s;
 
-//     let r3 = &mut s;  // error
-//     println!("{} {}", r1, r2);
+fn add_emoji(tweet: &mut String) {
+    tweet.push_str(" 😂");
+}
 
-//     println!("{}", r3);
-// }
+fn analyse_tweet(tweet: &String) -> TweetType {
+    if tweet.contains("!") {
+        return TweetType::Excited;
+    } else if tweet.contains("?") {
+        return TweetType::Question;
+    }
+    return TweetType::Regular;
+}
+fn print_previw(tweet: &String) {
+    let preview = &tweet[..std::cmp::min(10, tweet.len())];
+    println!("Preview : {}", preview);
+}
+
+fn calculate_len(tweet: &String) {
+    let len = tweet.len();
+    println!("Length : {}", len)
+}
+
+fn main() {
+    println!("Enter your tweet:");
+
+    let mut tweet = String::new(); // mutable string to store tweet
+
+    io::stdin()
+        .read_line(&mut tweet) // read from standard tweet
+        .expect("Failed to read line");
+
+    // Remove trailing newline characters
+    let mut tweet = tweet.trim().to_string(); // returns a &str
+
+    println!("Original: {}", tweet);
+    calculate_len(&tweet);
+    print_previw(&tweet);
+    add_emoji(&mut tweet);
+    let tweet_type = analyse_tweet(&tweet);
+
+    match tweet_type {
+        TweetType::Excited => println!("Tweet is excited"),
+        TweetType::Question => println!("Tweet is a Question."),
+        TweetType::Regular => println!("Tweet is Regular."),
+    }
+
+  
+}
