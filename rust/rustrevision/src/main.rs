95f097f4232090f2a317d5a7207fe304f3d87d3c
tachyonok <tachyonparticle123@gmail.com>
2025-07-18 12:11:55 +0530

    mutable borrowing

--- rust/rustrevision/src/main.rs
+++ rust/rustrevision/src/main.rs
@@ -2,6 +2,8 @@
 
 fn main() {
     let a = String::from("Solana is amazing");
+    let mut s = String::from("Btc is rocket");
+    add_emoji(&mut s);
     let b = &a;
     print_length(b);
 }
@@ -9,4 +11,9 @@ fn main() {
 fn print_length(s: &String) {
     let length = s.len();
     print!("length of the string {}", length);
+}
+
+fn add_emoji(s: &mut String) {
+    s.push_str(" 🚀");
+    println!("muted String {}", s);
 }
