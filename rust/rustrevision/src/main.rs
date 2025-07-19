d85e2e5c9a2791f044e10e620a89056ece5c12b8
tachyonok <tachyonparticle123@gmail.com>
2025-07-19 12:58:20 +0530

    enum with pattern matching

--- rust/rustrevision/src/main.rs
+++ rust/rustrevision/src/main.rs
@@ -1,27 +1,27 @@
-struct Student {
-    name: String,
-    grade: u8,
-    subjects: Vec<String>,
+// enum with pattern matching
+enum TweetType {
+    Text(String),
+    Image(String),
+    Video { url: String, duration: u32 },
 }
-impl Student {
-    fn add_subject(&mut self) {
-        self.subjects.push(String::from("Hindi"));
-    }
 
-    fn display(&self) {
-        println!(
-            "{} has grade {} and has Subjects {:?}",
-            self.name, self.grade, self.subjects
-        )
+fn analyze_tweet(tweet: TweetType) {
+    match tweet {
+        TweetType::Text(tweet) => println!("Tweet is {}", tweet),
+        TweetType::Image(img) => println!("image is {}", img),
+        TweetType::Video { url, duration } => {
+            println!("Video url is {} and is {} long", url, duration)
+        }
     }
 }
 
 fn main() {
-    let mut s1 = Student {
-        name: String::from("Harish"),
-        grade: 97,
-        subjects: vec![String::from("Maths"), String::from("Science")],
-    };
-    s1.add_subject();
-    s1.display();
+    analyze_tweet(TweetType::Text(String::from(
+        "Rust is such a great language",
+    )));
+    analyze_tweet(TweetType::Image(String::from("Some ur")));
+    analyze_tweet(TweetType::Video {
+        url: (String::from("some url")),
+        duration: (10),
+    });
 }
