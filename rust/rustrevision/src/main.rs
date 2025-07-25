6aae76624732e250e98abf783dd5dda0730865ea
tachyonok <tachyonparticle123@gmail.com>
2025-07-25 17:21:07 +0530

    hashmap

--- rust/rustrevision/src/main.rs
+++ rust/rustrevision/src/main.rs
@@ -1,16 +1,27 @@
-fn parse_tweet(tweet: &str) -> Result<usize, String> {
-    if tweet.len() >= 280 {
-        Err(String::from("Tweet is above 280 characters"))
-    } else {
-        Ok(tweet.len())
-    }
+use std::collections::HashMap;
+
+fn s(text: &str) -> String {
+    text.to_string()
 }
 
 fn main() {
-    let tweet =
-        String::from("Hey there i am thinking about doin 70 hr week can i do it i really want to");
-    match parse_tweet(&tweet) {
-        Ok(length) => println!("The length is {}", length),
-        Err(msg) => println!("Error is {}", msg),
+    let mut tweet_map = HashMap::new();
+
+    tweet_map.insert(1, s("hey"));
+    tweet_map.insert(2, s("Yo yo yo"));
+    tweet_map.insert(3, s("hani sengh"));
+
+    for (i, v) in &tweet_map {
+        println!("The key value pair is {} = {}", i, v)
     }
+
+    match tweet_map.get(&2) {
+        Some(tweet) => println!("tweet 2 is :{}", tweet),
+        None => println!("Tweet not found"),
+    }
+
+    tweet_map.remove(&1);
+
+    let tweet_count = tweet_map.len();
+    println!("No of Tweeets left {}", tweet_count);
 }
